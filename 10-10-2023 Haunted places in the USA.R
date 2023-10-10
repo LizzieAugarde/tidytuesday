93bd231f9bb4776a50c63bd16bda64ca9059ee98
Elizabeth Augarde <ELAU2@hscic.gov.uk>
2023-10-10 17:04:11 +0100

    10/10/2023 added basic thematic map of the number of sites per 1000 square miles

--- 10-10-2023 Haunted places in the USA.R
+++ 10-10-2023 Haunted places in the USA.R
@@ -12,7 +12,7 @@ number_by_state <- haunted_places %>%
   arrange(desc(number_places)) %>%
   mutate(state = factor(state, levels = state))
 
-ggplot(aes(x = state, y = number_places)) +
+number_by_state_graph <- ggplot(number_by_state, aes(x = state, y = number_places)) +
     geom_bar(stat = "identity", fill = "#3eb0d2") +
     labs(x = "State", y = "Number of haunted places") +
     scale_y_continuous(expand = c(0, 0), limits = c(0, 700),
@@ -35,10 +35,40 @@ state_size <- table_content %>%
   mutate(area = gsub("\\s*\\([^\\)]+\\)","", as.character(`Land Area (sq mi)`))) %>%
   mutate(area = as.numeric(gsub(",", "", area)))
 
-number_by_state <- left_join(number_by_state, state_size, by = c("state" = "State"))
+number_by_state_size <- left_join(number_by_state, state_size, by = c("state" = "State"))
 
-number_by_state <- number_by_state %>%
-  mutate(number_per_sq_mile = (number_places/area)*1000)
+number_by_state_size <- number_by_state_size %>%
+  mutate(number_per_sq_mile = (number_places/area)*1000) %>%
+  mutate(number_per_sq_mile_cat = case_when(number_per_sq_mile < 10 ~ "Less than 10", 
+                                            number_per_sq_mile >= 10 & number_per_sq_mile < 20 ~ "10 to 20",
+                                            number_per_sq_mile >= 20 & number_per_sq_mile < 40 ~ "20 to 40",
+                                            number_per_sq_mile >= 40 & number_per_sq_mile < 60 ~ "40 to 60",
+                                            TRUE ~ "More than 60"))
 
-test
+number_by_state_size_graph <- ggplot(number_by_state_size, aes(x = state, y = number_per_sq_mile)) +
+  geom_bar(stat = "identity", fill = "#3eb0d2") +
+  labs(x = "State", y = "Number of haunted places per 1000 square miles") +
+  scale_y_continuous(expand = c(0, 0)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle=90, hjust = 1))
          
+number_by_state_size_graph
+
+
+#number of places by state area size plotted on a basic thematic
+library(tigris) # for boundary data
+library(tmap) # for mapping functions
+
+states <- states(cb = TRUE) # downloading states boundary file
+
+states <- left_join(states, number_by_state_size, by = c("NAME" = "state"))
+
+states <- states %>% 
+  filter(!is.na(number_per_sq_mile), !NAME %in% c("Hawaii", "Alaska"))
+
+tm_shape(states) + 
+  tm_fill(col = "number_per_sq_mile_cat") +
+  tm_borders()
+
+
+#number of places by state area size plotted on a hexagonal thematic map 
