8114808d0d1cf59545bf1c87f9f889dc1cd5f40d
Elizabeth Augarde <ELAU2@hscic.gov.uk>
2023-10-26 17:12:44 +0100

    24/10/2023 data wrangling - grouping into risk by age, sex and prior condition

---
+++ 24-10-2023 Patient Risk Profiles.R
@@ -0,0 +1,28 @@
+library(tidytuesdayR)
+library(tidyverse)
+
+### 24/10/2023 - Patient Risk Profiles
+
+patient_risk_profiles <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-10-24/patient_risk_profiles.csv')
+
+
+# grouping into risk by age group, sex and prior condition
+patient_risk_profiles <- patient_risk_profiles %>%
+  pivot_longer(cols = starts_with("age group"),
+               names_to = "age_group", 
+               values_to = "number_age_group") %>%
+  pivot_longer(cols = starts_with("Sex"), 
+               names_to = "sex", 
+               values_to = "number_sex") %>%
+  pivot_longer(cols = ends_with(" in prior year"), 
+               names_to = "prior_condition", 
+               values_to = "number_prior_condition") %>%
+  mutate(age_group = trimws(gsub('age group: ', '', age_group)), 
+         sex = gsub('Sex = ', '', sex)) %>%
+  mutate(prior_condition = ifelse(number_prior_condition == 0, paste0("No ", prior_condition), prior_condition)) %>%
+  filter(number_age_group == 1, number_sex == 1) %>%
+  select(-c(number_age_group, number_sex, number_prior_condition, personId))
+
+mean_risk_by_group <- patient_risk_profiles %>%
+  group_by(age_group, sex, prior_condition) %>%
+  summarise(across(everything(), list(mean)))
